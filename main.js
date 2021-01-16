b057be97309770de7197a7483a85ccb3640203bb
anonymusdman <77517905+anonymusdman@users.noreply.github.com>
2021-01-15 16:54:19 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function fdrx7info() {
+    window.alert("The Mazda RX-7 is a front/mid-engine, rear-wheel-drive, rotary engine-powered sports car that was manufactured and marketed by Mazda from 1978 to 2002 across three generations, all of which made use of a compact, lightweight Wankel rotary engine.")
+    
+} 
